1da95c022101446b4c4b4fa0e26c2690cc9b24d7
Davian Ramsay <davian.ramsay@revature.net>
2021-10-05 11:46:01 -0400

    created API and individual layers

---
+++ ShopService/DataLayerDbContext/Models/Seasonal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataLayerDbContext.Models
+{
+    public partial class Seasonal
+    {
+        public Seasonal()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int SeasonalId { get; set; }
+        public string SeasonalName { get; set; }
+        public DateTime SeasonalStartDate { get; set; }
+        public DateTime SeasonalEndDate { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
