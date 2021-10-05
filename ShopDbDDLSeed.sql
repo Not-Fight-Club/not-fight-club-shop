2d33fdc71e07969364d61cfa7c91e50b498799d8
Davian Ramsay <davian.ramsay@revature.net>
2021-10-05 12:58:13 -0400

    db Seed

---
+++ ShopDbDDLSeed.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE ShopDB
+use ShopDB
+
+
+CREATE TABLE Seasonal(
+SeasonalId int not null identity(1,1) primary key,
+SeasonalName varchar(50) not null,
+SeasonalStartDate Datetime2 not null,
+SeasonalEndDate Datetime2 not null
+)
+
+CREATE TABLE Product(
+ProductId int not null identity (1,1) primary key,
+SeasonalId int FOREIGN KEY REFERENCES Seasonal (SeasonalId),
+ProductName varchar(50) not null,
+ProductPrice decimal(19,4) not null,
+ProductDescription varchar(100) not null,
+ProductDiscount decimal (19,4)
+)
+
+ALTER TABLE Product
+ALTER COLUMN ProductDescription varchar(500) not null
+
+
+CREATE TABLE UserProduct(
+UserProductId int primary key identity(1,1) not null,
+UserId uniqueIdentifier,
+ProductId int FOREIGN KEY REFERENCES Product (ProductId) not null
+)
+
+
