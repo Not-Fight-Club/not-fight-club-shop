090bb249e0ef4fc7d5bcf8df3face09d18349691
Simran MAnandhar <simran.manandhar@revature.net>
2021-10-06 18:01:18 -0400

    add logic for not enough bucks

--- ShopService/BusinessLayer/Discount.cs
+++ ShopService/BusinessLayer/Discount.cs
@@ -7,25 +7,25 @@ using ModelsLayer.Models;
 
 namespace BusinessLayer
 {
-    public static class Discount
+  public static class Discount
+  {
+
+
+    public static decimal DiscountedCost(decimal productPrice, decimal productDiscount)
     {
-             
-       
-        public static decimal DiscountedCost(decimal productPrice, decimal productDiscount)
-        {
-            decimal discountedTotal; //what the total price will be after the discount has been applied
-            decimal actualDiscunt;//this stores the amount that will be discounted from the purchase price 
-            Product p = new Product();
+      decimal discountedTotal; //what the total price will be after the discount has been applied
+      decimal actualDiscunt;//this stores the amount that will be discounted from the purchase price 
+      Product p = new Product();
 
 
-            p.ProductPrice = productPrice;
-            p.ProductDiscount = productDiscount;
+      p.ProductPrice = productPrice;
+      p.ProductDiscount = productDiscount;
 
-            actualDiscunt = productPrice * productDiscount;
-            discountedTotal = productPrice - actualDiscunt;
+      actualDiscunt = productPrice * productDiscount;
+      discountedTotal = productPrice - actualDiscunt;
 
-            return discountedTotal;
-        
-        }
+      return discountedTotal;
+
     }
+  }
 }
