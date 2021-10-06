21d475d278fb30e206804f547e0112b6d508b1a4
Simran MAnandhar <simran.manandhar@revature.net>
2021-10-05 20:18:21 -0400

    Add Product Repo, Mapper, ViewProduct and Controller

--- ShopService/ModelsLayer/ViewModels/ViewProduct.cs
+++ ShopService/ModelsLayer/ViewModels/ViewProduct.cs
@@ -5,21 +5,28 @@ using System.Collections.Generic;
 
 namespace ModelsLayer.ViewModels
 {
-    public class ViewProduct
-    {
-        public ViewProduct()
-        {
-            UserProducts = new HashSet<UserProduct>();
-        }
+  public class ViewProduct
+  {
+    public ViewProduct() { }
 
-        public int ProductId { get; set; }
-        public int? SeasonalId { get; set; }
-        public string ProductName { get; set; }
-        public decimal ProductPrice { get; set; }
-        public string ProductDescription { get; set; }
-        public decimal? ProductDiscount { get; set; }
+    // public ViewProduct(int productId, int? seasonalId, string productName, decimal productPrice, string productDescription, decimal? productDiscount)
+    // {
+    //   ProductId = productId;
+    //   SeasonalId = seasonalId;
+    //   ProductName = productName;
+    //   ProductPrice = productPrice;
+    //   ProductDescription = productDescription;
+    //   ProductDiscount = productDiscount;
+    // }
 
-        public virtual Seasonal Seasonal { get; set; }
-        public virtual ICollection<UserProduct> UserProducts { get; set; }
-    }
+    public int ProductId { get; set; }
+    public int? SeasonalId { get; set; }
+    public string ProductName { get; set; }
+    public decimal ProductPrice { get; set; }
+    public string ProductDescription { get; set; }
+    public decimal? ProductDiscount { get; set; }
+
+    // public virtual Seasonal Seasonal { get; set; }
+    // public virtual ICollection<UserProduct> UserProducts { get; set; }
+  }
 }
