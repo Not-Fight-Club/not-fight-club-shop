e3a962e66c84b5b12a69d3f5b3df119b5904a1b5
Jeffrey Wright <jeffrey.wright@revature.net>
2021-10-15 18:48:56 -0400

    Added a holiday season and a Christmas balloon

--- GetAllSeasonalItems_SP.sql
+++ GetAllSeasonalItems_SP.sql
@@ -2,11 +2,27 @@
 INSERT INTO SEASONAL (SeasonalName, SeasonalStartDate, SeasonalEndDate)
 VALUES ('Halloween 2021', '9-1-2021', '11-1-2021');
 
+INSERT INTO SEASONAL (SeasonalName, SeasonalStartDate, SeasonalEndDate)
+VALUES ('Christmas 2021', '11-1-2021', '1-1-2022');
+
 INSERT INTO PRODUCT (ProductName, ProductPrice, ProductDescription)
 VALUES ('Balloon', 20, 'You can hang balloons anywhere');
 
 INSERT INTO PRODUCT (SeasonalId, ProductName, ProductPrice, ProductDescription)
 VALUES (1, 'Spooky Balloon', 20, 'A spooky balloon for Halloween');
+
+INSERT INTO PRODUCT (SeasonalId, ProductName, ProductPrice, ProductDescription)
+VALUES (2, 'Christmas Balloon', 20, 'A festive balloon for those who celebrate Christmas');
+
+INSERT INTO SEASONAL (SeasonalName, SeasonalStartDate, SeasonalStartDate)
+VALUES('Christmas 2021', '11-1-2021', '1-1-2021');
+
+UPDATE SEASONAL
+SET SeasonalName = 'Holidays 2021', SeasonalStartDate = '11-1-2021', SeasonalEndDate = '1-1-2022'
+WHERE SeasonalId = 2;
+
+SELECT *
+FROM SEASONAL;
 GO
 
 --Stored Procedure to create
