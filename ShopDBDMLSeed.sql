969c53540cec7e9d4243b059c338066221a0345f
BrianaWynnRevature <89043391+BrianaWynnRevature@users.noreply.github.com>
2021-10-19 13:37:58 -0700

    Create ShopDBDMLSeed.sql
    Not finished yet. Still need to add more data

---
+++ ShopDBDMLSeed.sql
@@ -0,0 +1,21 @@
+INSERT INTO Category(Category)
+VALUES('Trait'),
+('Location'),
+('Edit Character'),
+('Weather'),
+('Weapon')
+
+
+INSERT INTO SEASONAL (SeasonalName, SeasonalStartDate, SeasonalEndDate)
+VALUES ('Halloween 2021', '9-1-2021', '11-1-2021');
+
+INSERT INTO PRODUCT (ProductName, ProductPrice, ProductDescription, CategoryId)
+VALUES ('Balloon', 2500, 'You can hang balloons anywhere', 5),
+('Pumpkin', 2500, '300 pound pumpkin that you can toss at your opponent to negate one of their arguments', 5),
+('Skeleton', 2500, 'A skeleton', 5),
+('Murder Mystery Dinner Party', 2000, 'A skeleton', 2),
+('Murder Mystery Dinner Party', 2000, 'A skeleton', 2),
+
+
+INSERT INTO PRODUCT (SeasonalId, ProductName, ProductDiscount, ProductPrice, ProductDescription, CategoryId)
+VALUES (1, 'Scary Mask',50, 50, 'A scary mask for Halloween', 5);
